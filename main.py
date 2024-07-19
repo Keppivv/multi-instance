b19f6d52f907a98a0f7f0faa911422d678c7d026
Keppi <141186032+Keppivv@users.noreply.github.com>
2024-07-19 14:54:39 +0300

    main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import ctypes, time
 
 def main():
     mutex = ctypes.windll.kernel32.CreateMutexW(None, True, "ROBLOX_singletonMutex")
-    print("all g")
+    print("Don't close me")
     while True:
         time.sleep(1)
 
